4156e7d4a031e5dd99faa9346ec361806a7b432b
Brook Nash <brooknullsh@gmail.com>
2025-05-21 20:06:31 +0100

    feat: argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, Subcommand};
+use env_logger::Builder;
+use log::LevelFilter;
+
+#[derive(Subcommand)]
+enum Commands {
+  Init,
+}
+
+#[derive(Parser)]
+struct Args {
+  #[command(subcommand)]
+  command: Commands,
+}
+
+fn init_log() {
+  Builder::from_default_env()
+    .filter_level(LevelFilter::Trace)
+    .format_timestamp(None)
+    .format_target(false)
+    .init();
+}
+
 fn main() {
-  println!("Hello, world!");
+  init_log();
+  let args = Args::parse();
+
+  match args.command {
+    Commands::Init => log::info!("Hello, world!"),
+  }
 }
